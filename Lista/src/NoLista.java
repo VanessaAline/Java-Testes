231b8881ba4376050fde056e92a43ed4808480df
VanessaAline <48366275+VanessaAline@users.noreply.github.com>
2019-06-05 22:51:53 -0300

    Add files via upload

---
+++ Lista/src/NoLista.java
@@ -0,0 +1,29 @@
+
+public class NoLista {
+	//atributos
+	private int valor;
+	private NoLista nextNoLista;
+	
+	//Construtor\\
+	public NoLista(int valor) {
+		this.valor = valor;
+		this.nextNoLista = null;
+	}
+
+	//Gets / Sets\\
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public NoLista getNextNoLista() {
+		return nextNoLista;
+	}
+
+	public void setNextNoLista(NoLista nextNoLista) {
+		this.nextNoLista = nextNoLista;
+	}
+}
